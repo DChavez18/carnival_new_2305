289ef5b5d48a40f066f047920a1c2433ae3d3278
Derek Chavez <frrenose17@yahoo.com>
2023-06-13 09:18:53 -0600

    test: add preferences test

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Visitor do
     expect(visitor1.height).to eq(54)
     expect(visitor1.spending_money).to eq(10)
   end
+
+  it "has no preferences and can add them to an array" do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    expect(visitor1.preferences).to eq([])
+
+    visitor1.add_preference(:gentle)
+    visitor1.add_preference(:thrilling)
+
+    expect(visitor1.preferences).to eq([:gentle, :thrilling])
+  end
 end
