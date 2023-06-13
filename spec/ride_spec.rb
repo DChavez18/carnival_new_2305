f63dcbcbfd39efc6ff552350afbe1c49ee1f4e20
Derek Chavez <frrenose17@yahoo.com>
2023-06-13 10:06:06 -0600

    test: add tests for total_revenue and spending money

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -27,7 +27,23 @@ RSpec.describe Ride do
     visitor2.add_preference(:gentle)
     ride1.board_rider(visitor1)
     ride1.board_rider(visitor2)
+
     expect(ride1.rider_log.first).to eq([visitor1, 1])
+  end
+
+  it "can check vistors spending money and check total revenue" do
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+
+    expect(visitor1.spending_money).to eq(8)
+    expect(visitor2.spending_money).to eq(4)
+    expect(ride1.total_revenue).to eq(3)
   end
 
 end
