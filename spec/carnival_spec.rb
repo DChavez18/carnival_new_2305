42b068cd6e418f9f35b8949875ffd1731895c34a
Derek Chavez <frrenose17@yahoo.com>
2023-06-13 11:00:46 -0600

    test: add rides test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe Carnival do
     expect(carnival.duration).to eq(10)
     expect(carnival.rides).to eq([])
   end
+
+  it "can add rides to an array" do
+    carnival = Carnival.new(10)
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+
+    expect(carnival.rides).to eq([ride1, ride2])
+  end
 end
