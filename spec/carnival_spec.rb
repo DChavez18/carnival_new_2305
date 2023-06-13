a9004c2c5f1521f169e88077db6c9a2b17cf894d
Derek Chavez <frrenose17@yahoo.com>
2023-06-13 11:05:29 -0600

    test: add popular test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -24,4 +24,27 @@ RSpec.describe Carnival do
 
     expect(carnival.rides).to eq([ride1, ride2])
   end
+
+  it "has a most popular ride" do
+    carnival = Carnival.new(10)
+    ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:thrilling)
+
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride2.board_rider(visitor3)
+
+    expect(carnival.most_popular_ride).to eq(ride1)
+  end
 end
